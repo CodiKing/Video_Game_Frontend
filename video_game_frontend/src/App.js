2408de88dca240c1995be3e0da3e254a9b3400a5
Rick Rieger <rickrieger36251@gmail.com>
2022-04-26 15:09:26 -0400

    added axios utility + useEffect call to BE

--- video_game_frontend/src/App.js
+++ video_game_frontend/src/App.js
@@ -1,10 +1,21 @@
-
+import React, { useEffect, useState } from 'react';
+import Axios from './utils/Axios';
 
 function App() {
+  useEffect(() => {}, []);
+
+  const getAllVideoGames = async () => {
+    try {
+      const res = await Axios.get('/Games');
+      console.log(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div>
       <h3>Test!</h3>
-      
     </div>
   );
 }
